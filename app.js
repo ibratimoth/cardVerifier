9d5364bf7a443d6961928b5d8d778f3ca7dc7fb7
ibratimoth <ibratimoth@gmail.com>
2025-08-08 11:31:59 +0300

    first commit

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('forbidden')
-})
+
 app.use('/', cardRoutes);
 
 const PORT = process.env.PORT || 3004;
